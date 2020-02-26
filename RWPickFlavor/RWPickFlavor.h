0fc271c87a599eb6ed9d5e8d1f25c49199837dc4
Applause Codes <ola@applause.codes>
2020-02-26 12:14:40 -0300

    Initial commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Marcello Chuahy on 26/02/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
